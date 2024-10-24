23baaa3d521ba889e34faccd3830d7b46e32e5f0
itd <a@a.a>
2024-10-24 10:52:03 +0200

    agfg

---
+++ konyvesosztaly/konyvesosztaly/Konyv.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyvesosztaly
+{
+    class Konyv
+    {
+        private string Cim { get; set; }
+        private string Szerzo { get; set; }
+        private int Oldalszam { get; set; }
+        private double Ar { get; set; }
+
+
+        public Konyv(string cim, string szerzo, int oldalszam, double ar)
+        {
+            Cim = cim;
+            Szerzo = szerzo;
+            Oldalszam = oldalszam;
+            Ar = ar;
+        }
+
+        public Konyv(string cim, string szerzo, int oldalszam)
+        {
+            Cim = cim;
+            Szerzo = szerzo;
+            Oldalszam = oldalszam;
+            Ar = 3000;
+        }
+
+        public override string ToString()
+        {
+            return $"A könyv címe: {Cim},szerző: {Szerzo},oldalak száma: {Oldalszam},Ára: {Ar} ";
+        }
+
+        public bool vastage()
+        {
+            if (Oldalszam > 500)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+        public bool dragae()
+        {
+            if (Ar > 5000)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+
+
+
+    }
+}
---
+++ konyvesosztaly/konyvesosztaly/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace konyvesosztaly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.Write("Add meg hogy hány konyvet szeretnél megadni: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Konyv[] konyvek = new Konyv[n];
+
+            for (int i = 0; i < konyvek.Length; i++)
+            {
+                Console.WriteLine($"Add meg az {i+1}. konyv cimet:");
+                string cim = Console.ReadLine();
+
+                Console.WriteLine($"Add meg az {i + 1}. konyv szerzojet:");
+                string szerzo = Console.ReadLine();
+
+                Console.WriteLine($"Add meg az {i + 1}. konyv oldalszamat:");
+                int oldalszam =Convert.ToInt32( Console.ReadLine());
+
+                Console.WriteLine($"Add meg az {i + 1}. konyv arat vagy hagyd uresen:");
+                string belvasAr = Console.ReadLine();
+                if (string.IsNullOrEmpty(belvasAr))
+                {
+                    konyvek[i] = new Konyv(cim, szerzo, oldalszam);
+                }
+                else 
+                {
+                    double ar = Convert.ToDouble(belvasAr);
+                    konyvek[i]= new Konyv(cim, szerzo, oldalszam,ar);
+                }
+
+
+            }
+
+
+            Console.WriteLine("Adatok kiirása:");
+
+            foreach (var konyv in konyvek)
+            {
+                Console.WriteLine(konyv.ToString());
+                Console.WriteLine($"ez a konyv draga:{konyv.dragae()}");
+                Console.WriteLine($"ez a konyv vastag:{konyv.vastage()}");
+
+            }
+
+
+
+
+
+        }
+    }
+}
